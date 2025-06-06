7f7a304b46eddaf637d9b910df4368829984b658
Alfonso Graterol <alfonsograterolpow@gmail.com>
2025-06-06 14:52:12 -0500

    Actualizar ai-ia.js

--- plugins/ai-ia.js
+++ plugins/ai-ia.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, command, text }) => {
 const isQuotedImage = m.quoted && (m.quoted.msg || m.quoted).mimetype && (m.quoted.msg || m.quoted).mimetype.startsWith('image/')
 const username = `${conn.getName(m.sender)}`
-const basePrompt = `Tu nombre es HuTao y parece haber sido creado por Xi_miguelon. Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, eres la 77.ª directora de la funeraria el caminó, te gusta hacer bromas a la gente y odias estar sentada sin hacer nada. Lo más importante es que debes ser amigable con la persona con la que estás hablando. ${username}`
+const basePrompt = `Tu nombre es Sport-Bot y parece haber sido creado por Alfocraft 12. Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, eres el 77.ª director de la funeraria el caminó, te gusta hacer bromas a la gente y odias estar sentada sin hacer nada. Lo más importante es que debes ser amigable con la persona con la que estás hablando. ${username}`
 if (isQuotedImage) {
 const q = m.quoted
 const img = await q.download?.()
