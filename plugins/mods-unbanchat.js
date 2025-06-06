84fe47176fb9f803b3768778f0d094a19391f5bc
Alfonso Graterol <alfonsograterolpow@gmail.com>
2025-06-06 15:05:34 -0500

    Actualizar mods-unbanchat.js

--- plugins/mods-unbanchat.js
+++ plugins/mods-unbanchat.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => {
 if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, 'シ︎ *¡ESTE CHAT NO ESTÁ REGISTRADO!*', m, fake)
 let chat = global.db.data.chats[m.chat]
-if (!chat.isBanned) return conn.reply(m.chat, '✿︎ *¡Sport NO ESTÁ BANEADA EN ESTE CHAT!*', m, fake)
+if (!chat.isBanned) return conn.reply(m.chat, '✿︎ *¡Sport NO ESTÁ BANEADO EN ESTE CHAT!*', m, fake)
 chat.isBanned = false
-await conn.reply(m.chat, '❥︎ *¡Sport YA FUÉ DESBANEADA EN ESTE CHAT!*', m, fake)
+await conn.reply(m.chat, '❥︎ *¡Sport YA FUÉ DESBANEADO EN ESTE CHAT!*', m, fake)
 }
 handler.help = ['unbanchat'];
 handler.tags = ['mods'];
