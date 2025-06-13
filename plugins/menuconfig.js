f5a193cbd73c6bce4a8eef2803efea40bc5aaa43
Alfonso Graterol <alfonsograterolpow@gmail.com>
2025-06-13 16:18:54 -0500

    Update menuconfig.js

--- plugins/menuconfig.js
+++ plugins/menuconfig.js
@@ -1,8 +1,7 @@
 let handler = async (m, { isPrems, conn }) => {
 let time = global.db.data.users[m.sender].lastcofre + 0 // 36000000 10 Horas //86400000 24 Horas
 if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\𝚗𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`
-
-let img = 'https://qu.ax/jaxPN.jpg' 
+ 
 let texto = `» BIENVENIDO QUERIDO USUARIO!
 
 “ HOLA *, ESTE ES EL MENU DE SPORT-𝐁𝐎𝐓 😎 "
