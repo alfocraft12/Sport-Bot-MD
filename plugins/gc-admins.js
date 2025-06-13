34739c2221b024d87ffad1cf0c7fa5e29a464ec0
Alfonso Graterol <alfonsograterolpow@gmail.com>
2025-06-13 17:07:12 -0500

    Update gc-admins.js

--- plugins/gc-admins.js
+++ plugins/gc-admins.js
@@ -1,5 +1,4 @@
-const handler = async (m, {conn, participants, groupMetadata, args}) => {
-  const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => null) || '';
+const handler = async (m, { conn, participants, groupMetadata, args }) => {
   const groupAdmins = participants.filter((p) => p.admin);
   const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n');
   const owner = groupMetadata.owner || groupAdmins.find((p) => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net';
@@ -12,11 +11,21 @@ ${listAdmin}
 🍭 Mensaje adicional: ${oi}
 
 『✦』Levantense nojoda y vayas a trabajar cuerda de bagos.`.trim();
-  conn.sendFile(m.chat, pp, 'error.jpg', text, m, false, {mentions: [...groupAdmins.map((v) => v.id), owner]});
+
+  await conn.sendMessage(
+    m.chat,
+    {
+      text,
+      mentions: [...groupAdmins.map((v) => v.id), owner]
+    },
+    { quoted: m }
+  );
 };
+
 handler.help = ['admins <texto>'];
 handler.tags = ['grupo'];
 handler.customPrefix = /a|@/i;
 handler.command = /^(admins|@admins|dmins)$/i;
 handler.group = true;
+
 export default handler;
