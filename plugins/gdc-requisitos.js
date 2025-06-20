0613c549a038da7ef79cd5b7624dad1ad5787f06
Alfonso Graterol <alfonsograterolpow@gmail.com>
2025-06-20 18:38:46 -0500

    Create gdc-requisitos.js
    a new comand for rules of group

---
+++ plugins/gdc-requisitos.js
@@ -0,0 +1,35 @@
+var handler = async (m, { conn, participants }) => {
+  const users = participants.map(u => conn.decodeJid(u.id))
+
+  const mensaje = `
+📌 *Requisitos para permanecer en el clan*
+1. Prueba de comando (anexo)
+
+— _El admin_
+  `.trim()
+
+  // Caracter invisible para que las menciones queden ocultas
+  const invisible = String.fromCharCode(8206).repeat(850)
+
+  await conn.relayMessage(
+    m.chat,
+    {
+      extendedTextMessage: {
+        text: `${invisible}\n${mensaje}`,
+        contextInfo: {
+          mentionedJid: users
+        }
+      }
+    },
+    {}
+  )
+}
+
+handler.help = ['reglas']
+handler.tags = ['grupo']
+handler.command = ['reglas', 'rules', 'normas'] // Puedes cambiar los nombres
+
+handler.group = true
+handler.admin = true
+
+export default handler
