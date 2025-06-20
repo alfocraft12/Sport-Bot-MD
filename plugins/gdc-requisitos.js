cc8ce2dfde56081e7934f056c7126f2019091ada
Alfonso Graterol <alfonsograterolpow@gmail.com>
2025-06-20 18:56:30 -0500

    Update gdc-requisitos.js

--- plugins/gdc-requisitos.js
+++ plugins/gdc-requisitos.js
@@ -3,38 +3,30 @@ var handler = async (m, { conn, participants }) => {
 
   const mensaje = `
 📌 *REGLAS DEL GRUPO*
-1. Prueba.js
+1. No spam
+2. Respeto entre todos
+3. Prohibido contenido +18
+4. No hacer flood
+5. Cumple y sé parte del grupo 👍
 
 — _El admin_
   `.trim()
 
-  const fakeMsg = {
-    key: {
-      fromMe: false,
-      participant: "0@s.whatsapp.net",
-      remoteJid: m.chat
-    },
-    message: {
-      conversation: mensaje
-    }
-  }
+  // Caracter invisible para que las menciones queden ocultas
+  const invisible = String.fromCharCode(8206).repeat(850)
 
-  const msg = conn.cMod(
+  await conn.relayMessage(
     m.chat,
-    generateWAMessageFromContent(m.chat, {
+    {
       extendedTextMessage: {
-        text: mensaje,
+        text: `${invisible}\n${mensaje}`,
         contextInfo: {
           mentionedJid: users
         }
       }
-    }, { quoted: null, userJid: conn.user.id }),
-    mensaje,
-    conn.user.jid,
-    { mentions: users }
+    },
+    {}
   )
-
-  await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
 }
 
 handler.help = ['requisitos']
