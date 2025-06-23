ac38bf9af12d95152e6ba7430ede8785c4f04cad
Alfonso Graterol <alfonsograterolpow@gmail.com>
2025-06-23 15:07:16 -0500

    Sport-Bot-MD

--- plugins/gdc-requisitos.js
+++ plugins/gdc-requisitos.js
@@ -1,24 +1,62 @@
+import { promises as fs } from 'fs'
+import path from 'path'
+
 var handler = async (m, { conn }) => {
   const mensaje = `
-📌 *REGLAS DEL GRUPO*
-1. No spam
-2. Respeto entre todos
-3. Prohibido contenido +18
-4. No hacer flood
-5. Cumple y sé parte del grupo 👍
+*GUERRA DE CLANES 🗣*
+————————————
 
-— _El admin_
+1. HORARIOS 🕙: 
+
+- *🇻🇪 Venezuela:*
+ 8:00 pm - 12:00 pm
+
+- *🇨🇴 Colombia:*
+ 7:00 pm - 11:00 pm
+
+- *🇲🇽 México:* 
+ 6:00 pm - 10:00 pm
+
+
+//////////////////////
+
+
+2. *Días de guerra 🗓:*
+
+- Miércoles 
+
+- Sábados
+
+- Domingos
+
+
+//////////////////////
+
+
+3. *Reglas 🚨:*
+
+- Se Prohíbe *jugar otro modo* durante la guerra de clanes
+
+- El estar activo y no aceptar también se considera como no aportar
+
+- El *insultar a los jugadores* en el juego solo por ser "malos" también está prohibido (nada de toxicidad)
+
+- Se necesitan personas dedicadas a la guerra (así sea los sábados y domingos)
+
+————————————
+Y con esto le damos las gracias y bienvenidas a todos, espero y la pasen bien ❤‍🩹 
+————————————
+> _*ATT:* Admins_
   `.trim()
 
-  await conn.relayMessage(
-    m.chat,
-    {
-      extendedTextMessage: {
-        text: mensaje
-      }
-    },
-    {}
-  )
+  // Ruta relativa al archivo desde el mismo nivel del handler
+  const imgPath = path.join('./src', 'guerra-de-clanes.jpeg')
+  const buffer = await fs.readFile(imgPath)
+
+  await conn.sendMessage(m.chat, {
+    image: buffer,
+    caption: mensaje
+  }, { quoted: m })
 }
 
 handler.help = ['requisitos']
@@ -29,4 +67,3 @@ handler.group = true
 handler.admin = true
 
 export default handler
-
