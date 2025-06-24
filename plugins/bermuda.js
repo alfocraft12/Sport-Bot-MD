443f8a3baeebaceb86507acdb64cd2eea1e680cd
Alfonso Graterol <alfonsograterolpow@gmail.com>
2025-06-24 18:09:55 -0500

    Sport-Bot-MD

--- plugins/bermuda.js
+++ plugins/bermuda.js
@@ -1,22 +1,6 @@
 let handler = async (m, { conn }) => {
-  let fake = {
-    forwardingScore: 999,
-    isForwarded: true,
-    externalAdReply: {
-      title: "Sport-Bot Noticias",
-      body: "🗺️ Mapa competitivo: Bermuda",
-      mediaType: 1,
-      previewType: 0,
-      thumbnailUrl: null,
-      renderLargerThumbnail: true,
-      showAdAttribution: true,
-      sourceUrl: "https://whatsapp.com/channel/0029Vb3yM0T2v1ItNDrGIY3v" // ⚠️ Reemplaza con el link real de tu canal
-    }
-  }
-
   let imagen = './src/freefire/bermuda.jpeg'
-
-  await conn.sendFile(m.chat, imagen, 'bermuda.jpeg', '', m, false, fake)
+  await conn.sendFile(m.chat, imagen, 'bermuda.jpeg', '', m, false, global.channelRD)
 }
 
 handler.command = ['bermuda']
