d148e609cf00b2377f08324b5b1904760e20e021
Alfonso Graterol <alfonsograterolpow@gmail.com>
2025-06-24 22:01:00 -0500

    Sport-Bot-MD

--- plugins/bermuda.js
+++ plugins/bermuda.js
@@ -1,28 +1,28 @@
-let handler = async (m, { conn, usedPrefix, command}) => {
+let handler = async (m, { conn, usedPrefix, command }) => {
 
-let fkontak = {
-  "key": {
-    "participants": "0@s.whatsapp.net",
-    "remoteJid": "status@broadcast",
-    "fromMe": false,
-    "id": "Halo"
-  },
-  "message": {
-    "contactMessage": {
-      "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-    }
-  },
-  "participant": "0@s.whatsapp.net"
-}
+  let fkontak = {
+    key: {
+      participants: "0@s.whatsapp.net",
+      remoteJid: "status@broadcast",
+      fromMe: false,
+      id: "Halo"
+    },
+    message: {
+      contactMessage: {
+        vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+      }
+    },
+    participant: "0@s.whatsapp.net"
+  }
 
-let yaemori = `let handler = async (m, { conn }) => {
+  let yaemori = `let handler = async (m, { conn }) => {
   let imagen = './src/freefire/bermuda.jpeg'
 }
 
 ${global.md}`.trim()
 
-await conn.reply(m.chat, yaemori, m, fkontak)
-
+  await delay(1000) // <-- para prevenir el rate-overlimit
+  await conn.reply(m.chat, yaemori, m, fkontak)
 }
 
 handler.help = ['mapas']
@@ -39,4 +39,8 @@ function clockString(ms) {
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
   return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
