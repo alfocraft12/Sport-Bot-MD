3f90b30a7218159b4241b8311e0290e9ef70f957
Alfonso Graterol <alfonsograterolpow@gmail.com>
2025-06-24 21:55:19 -0500

    Sport-Bot-MD

--- plugins/bermuda.js
+++ plugins/bermuda.js
@@ -1,8 +1,25 @@
-let handler = async (m, { conn }) => {
-  let imagen = './src/freefire/bermuda.jpeg'
-  await conn.sendFile(m.chat, imagen, 'bermuda.jpeg', '', m, false, global.channelRD)
-}
+let handler = async (m, { conn, usedPrefix, command}) => {
 
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+
+let yaemori = `let handler = async (m, { conn }) => {
+  let imagen = './src/freefire/bermuda.jpeg
+
+${global.md}`.trim()
+await conn.reply(m.chat, yaemori, m, fake)
+
+}
+handler.help = ['mapas']
+handler.tags = ['main']
 handler.command = ['bermuda']
-handler.group = true
+handler.register = false
 export default handler
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+function clockString(ms) {
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
