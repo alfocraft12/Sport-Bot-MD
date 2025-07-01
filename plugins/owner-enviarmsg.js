02901670826f2ef76ede56dea577b3dc59796ddd
Alfonso Graterol <alfonsograterolpow@gmail.com>
2025-07-01 18:44:24 -0500

    Sport-Bot-MD

--- plugins/owner-enviarmsg.js
+++ plugins/owner-enviarmsg.js
@@ -1,40 +1,46 @@
-/*const idgroup = "120363351999685409@g.us"; 
-const reports = {}; 
+const idchannel = "120363402702484902@newsletter"; // ID de tu canal
+const reports = {};
 
 let handler = async (m, { conn, command, args }) => {
   if (command === 'test') {
     const contenido = args.join(' ');
 
     if (!contenido) {
-      return conn.reply(m.chat, `✿ Por favor, proporciona un reporte.`, m);
+      return conn.reply(m.chat, `🙌 Por favor, proporciona un reporte.`, m);
     }
 
     const senderName = await conn.getName(m.sender);
-    const reportText = `👤 *Usuario:* ${senderName || 'Anónimo'}\n📝 *Reporte:* ${contenido || 'Sin descripción'}`;
+    const reportText = `👤 *Usuario:* ${senderName || 'Anónimo'}\n📝 *Reporte:* ${contenido || 'Sin descripción'}\n📱 *Número:* @${m.sender.split('@')[0]}`;
 
-    await conn.sendMessage(idgroup, { text: `✿ Nuevo reporte:\n\n${reportText}\n\nUsuario: @${m.sender.split('@')[0]}`, mentions: [m.sender] });
+    const msg = await conn.sendMessage(idchannel, { text: `✿ Nuevo reporte:\n\n${reportText}` });
 
-    await conn.reply(m.chat, `❀ Tu reporte ha sido enviado para revisión.`, m);
+    // Guardar referencia del mensaje en caso de respuesta
+    reports[msg.key.id] = {
+      user: m.sender,
+      contenido
+    }
+
+    await conn.reply(m.chat, `🙌 Tu reporte ha sido enviado al canal para revisión.`, m);
 
   } else if (command === 'responder') {
     if (!m.quoted || !reports[m.quoted.key.id]) {
-      return conn.reply(m.chat, `✿ No se encontró el reporte para responder.`, m);
+      return conn.reply(m.chat, `❌ No se encontró el reporte para responder.`, m);
     }
 
     const response = args.join(' ');
 
     if (!response) {
-      return conn.reply(m.chat, `✿ Por favor, proporciona una respuesta.`, m);
+      return conn.reply(m.chat, `🙏 Por favor, proporciona una respuesta.`, m);
     }
 
     const report = reports[m.quoted.key.id];
-    await conn.reply(report.user, `❀ Respuesta a tu reporte: ${response}`, null);
+    await conn.reply(report.user, `📣 Respuesta a tu reporte: ${response}`, null);
 
-    await conn.sendMessage(idgroup, { text: `✿ Respuesta al reporte:\n\n${response}\n\nReporte original: ${report.contenido}\n\nRespondido por: @${m.sender.split('@')[0]}`, mentions: [m.sender] });
-
+    await conn.sendMessage(idchannel, {
+      text: `❗ Respuesta enviada:\n\n🗨️ *Respuesta:* ${response}\n📋 *Reporte original:* ${report.contenido}\n👤 *Respondido por:* @${m.sender.split('@')[0]}`
+    });
   }
 }
 
 handler.command = ['test', 'responder'];
-
-export default handler;*/
+export default handler;
