e62dff96259056671d3c2a2eb4922b47bdb49965
Alfonso Graterol <alfonsograterolpow@gmail.com>
2025-07-01 20:59:44 -0500

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
   await conn.sendMessage(m.chat, { text: '🧪 Test de envío directo, me vez????' })
 }
-handler.command = ['test']
+handler.command = ['tester']
 export default handler
