1f94bc4208c1695c77e5bf47627dfc477a6210a7
Alfonso Graterol <alfonsograterolpow@gmail.com>
2025-07-10 23:31:10 -0500

    Sport-Bot-MD

--- plugins/menu-lista.js
+++ plugins/menu-lista.js
@@ -459,7 +459,7 @@ id: `${prefijo}buyall`
         }
     };
     function _0x2daf() {
-    const _0x4c1076 = ['namedoc', 'social', '1017dFLzIP', '11680bWFOeX', 'sendMessage', '1FnTozH', '6qNtNxK', '445374chjKag', '2096504ySppGm', '627669MaFyqj', 'readFileSync', 'En breve se enviará el menú...', '374160lMCurS', '356228pujvOS', './Menu.jpg', '1019845zOpQQK', 'pdf', 'chat'];
+    const _0x4c1076 = ['namedoc', 'social', '1017dFLzIP', '11680bWFOeX', 'sendMessage', '1FnTozH', '6qNtNxK', '445374chjKag', '2096504ySppGm', '627669MaFyqj', 'readFileSync', 'En breve se enviará el menú...', '374160lMCurS', '356228pujvOS', 'src/menu_en.png', '1019845zOpQQK', 'pdf', 'chat'];
     _0x2daf = function() {
         return _0x4c1076;
     };
