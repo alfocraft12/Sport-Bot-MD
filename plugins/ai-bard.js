157837963b7b87761d1c118cd010d0441a6e6c6d
Alfonso Graterol <alfonsograterolpow@gmail.com>
2025-07-15 18:57:28 -0500

    Sport-Bot-MD

--- plugins/ai-bard.js
+++ plugins/ai-bard.js
@@ -1,23 +1,30 @@
 import fetch from 'node-fetch'
 
-var handler = async (m, { text,  usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `Ꙭ *Ingresé una petición*\n\nEjemplo, ${usedPrefix + command} Conoces a DcA Barbie?`, m, rcanal)
-try {
-await m.react('🕒')
-conn.sendPresenceUpdate('composing', m.chat)
-var apii = await fetch(`https://aemt.me/bard?text=${text}`)
-var res = await apii.json()
-await conn.reply(m.chat, res.result, m, rcanal)
-await m.react('✅️')
-} catch (error) {
-await m.react('✖️')
-console.error(error)
-return conn.reply(m.chat, '☢︎︎ *Ocurrió un fallo*', m, rcanal)
-}}
+var handler = async (m, { text, usedPrefix, command }) => {
+  if (!text) return conn.reply(m.chat, `Ꙭ *Ingresa una petición*\n\nEjemplo: ${usedPrefix + command} Conoces a DcA Barbie?`, m, rcanal)
+
+  try {
+    await m.react('🕒')
+    conn.sendPresenceUpdate('composing', m.chat)
+
+    const response = await fetch(`https://api.akuari.my.id/ai/bard?chat=${encodeURIComponent(text)}`)
+    const json = await response.json()
+
+    if (!json || !json.respon) throw new Error('❌ No se recibió respuesta válida.')
+
+    await conn.reply(m.chat, json.respon, m, rcanal)
+    await m.react('✅️')
+
+  } catch (error) {
+    await m.react('✖️')
+    console.error(error)
+    return conn.reply(m.chat, '☢︎︎ *Ocurrió un fallo al procesar tu mensaje.*', m, rcanal)
+  }
+}
 
 handler.command = ['bard']
 handler.help = ['bard']
-handler.group = true;
+handler.group = true
 handler.register = false
 handler.tags = ['ai']
 handler.premium = false
