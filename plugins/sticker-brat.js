d7e83c5b00726384c3406d2725c947c2925c8e08
Alfonso Graterol <alfonsograterolpow@gmail.com>
2025-08-25 18:12:31 -0500

    Sport-Bot-MD

--- plugins/sticker-brat.js
+++ plugins/sticker-brat.js
@@ -1,18 +1,11 @@
 import axios from 'axios';
-import fs from 'fs';
-import path from 'path';
-import sharp from 'sharp';
-import {
-    tmpdir
-} from 'os';
+
 const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 const fetchSticker = async (text, attempt = 1) => {
     try {
         const response = await axios.get(`https://kepolu-brat.hf.space/brat`, {
-            params: {
-                q: text
-            },
+            params: { q: text },
             responseType: 'arraybuffer',
         });
         return response.data;
@@ -26,52 +19,27 @@ const fetchSticker = async (text, attempt = 1) => {
     }
 };
 
-const handler = async (m, {
-    text,
-    conn
-}) => {
+const handler = async (m, { text, conn }) => {
     if (!text) {
         return conn.sendMessage(m.chat, {
-            text: `${emoji} Por favor ingresa el texto para hacer un sticker.`,
-        }, {
-            quoted: m
-        });
+            text: "⚠️ Ingresa un texto para hacer el sticker."
+        }, { quoted: m });
     }
 
     try {
         const buffer = await fetchSticker(text);
-        const outputFilePath = path.join(tmpdir(), `sticker-${Date.now()}.webp`);
-        await sharp(buffer)
-            .resize(512, 512, {
-                fit: 'contain',
-                background: {
-                    r: 255,
-                    g: 255,
-                    b: 255,
-                    alpha: 0
-                }
-            })
-            .webp({
-                quality: 80
-            })
-            .toFile(outputFilePath);
 
         await conn.sendMessage(m.chat, {
-            sticker: {
-                url: outputFilePath
-            },
-        }, {
-            quoted: fkontak
-        });
-        fs.unlinkSync(outputFilePath);
+            sticker: buffer
+        }, { quoted: m });
+
     } catch (error) {
         return conn.sendMessage(m.chat, {
-            text: `${msm} Ocurrio un error.`,
-        }, {
-            quoted: m
-        });
+            text: "❌ Ocurrió un error al generar el sticker."
+        }, { quoted: m });
     }
 };
+
 handler.command = ['brat'];
 handler.tags = ['sticker'];
 handler.help = ['brat *<texto>*'];
