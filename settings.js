33e3972c946fe6261d486b9484aecfc30ae6a659
Alfonso Graterol <alfonsograterolpow@gmail.com>
2025-08-25 16:25:12 -0500

    Sport-Bot-MD

---
+++ settings.js
@@ -0,0 +1,81 @@
+import fs from 'fs'
+import cheerio from 'cheerio'
+import fetch from 'node-fetch'
+import axios from 'axios'
+import moment from 'moment-timezone'
+
+global.botnumber = ''
+
+global.owner = [
+    ["5218711426787", "Propietario 👑", true],
+    ["5492916450307"],    
+    ["5218712620915"],
+    ["5351524614"]
+];
+
+global.mods = []
+global.suittag = []
+global.prems = []
+
+global.packsticker = '★彡( Ӿł_₥ł₲ɄɆⱠØ₦77ӾӾ )彡★'
+global.packname = '󠁖󠁖󠁖󠁖󠁖󠁖󠁻󠁻𝐇𝐮ⷡ𝐓ⷪ𝐚ⷮ𝐨-𝐌𝐃󠁂󠀧󠁂🈀'
+global.author = 'ɦʊȶǟօ-քʀօʏɛƈȶ'
+global.wm = 'ɦʊȶǟօ-քʀօʏɛƈȶ'
+global.titulowm = '★彡( Ӿł_₥ł₲ɄɆⱠØ₦77ӾӾ )彡★'
+global.titulowm2 = 'ɦʊȶǟօ-քʀօʏɛƈȶ'
+global.igfg = 'ɦʊȶǟօ-քʀօʏɛƈȶ'
+global.botname = 'ɦʊȶǟօ-քʀօʏɛƈȶ'
+global.dev = '★彡( Ӿł_₥ł₲ɄɆⱠØ₦77ӾӾ )彡★'
+global.textbot = 'ɦʊȶǟօ-քʀօʏɛƈȶ'
+global.gt = '𝐇𝐮ⷡ𝐓ⷪ𝐚ⷮ𝐨-𝐌𝐃󠁂󠀧󠁂🈀'
+global.namechannel = 'ɦʊȶǟօ-քʀօʏɛƈȶ'
+global.vs = 'V2.3'
+
+global.imagen1 = "https://stellarwa.xyz/files/1752422417390.jpg"
+global.imagen2 = "https://stellarwa.xyz/files/1752422509254.jpg"
+global.imagen3 = "https://stellarwa.xyz/files/1752422535890.jpg"
+global.imagen4 = "https://stellarwa.xyz/files/1752422562260.jpg"
+global.imagen5 = fs.readFileSync('./src/+18.jpg')
+global.imagen6 = "https://stellarwa.xyz/files/1752422642121.jpg"
+global.imagen7 = "https://stellarwa.xyz/files/1752422693806.jpg"
+global.imagen8 = "https://stellarwa.xyz/files/1752422716622.jpg"
+global.imagen9 = "https://stellarwa.xyz/files/1752422417390.jpg"
+global.imagen10 = "https://stellarwa.xyz/files/1752422642121.jpg"
+global.miniurl = "https://stellarwa.xyz/files/1752422642121.jpg"
+global.logo2 = "https://stellarwa.xyz/files/1752422642121.jpg"
+global.logo3 = "https://stellarwa.xyz/files/1752422642121.jpg"
+global.catalogo = "https://stellarwa.xyz/files/1752422642121.jpg"
+global.logo4 = "https://stellarwa.xyz/files/1752422642121.jpg"
+global.logo5 = "https://stellarwa.xyz/files/1752422642121.jpg"
+global.logo7 = "https://stellarwa.xyz/files/1752422642121.jpg"
+global.logo8 = "https://stellarwa.xyz/files/1752422642121.jpg"
+global.rule = fs.readFileSync('./src/rule.jpg')
+global.welcome = "https://stellarwa.xyz/files/1752422465230.jpg"
+global.adios = "https://stellarwa.xyz/files/1752426542029.jpg"
+
+global.photoSity = [imagen8, imagen1, imagen4, imagen6]
+
+global.languaje = 'Es'
+global.nameqr = 'HuTao'
+global.sessions = 'Session/Hutao'
+global.jadi = 'Session/SubBot'
+
+global.channel = {
+channel1: "120363420992828502@newsletter",
+channel2: "120363419171116775@newsletter",
+channel3: "120363387958443019@newsletter",
+channel4: "120363371018732371@newsletter"
+}
+
+global.gp4 = 'https://chat.whatsapp.com/LcifaLUrmww6CPT27IuSAa'
+global.gp1 = 'https://chat.whatsapp.com/GFyLX1dDDxI0utho5GBmJI'
+global.channel = 'https://whatsapp.com/channel/0029Vb7Ji66KbYMTYLU9km3p'
+global.md = 'https://github.com/CheirZ'
+global.correo = 'miguel.doce12000@outlook.com'
+global.cn = 'https://whatsapp.com/channel/0029VacDy0R6hENqnTKnG820'
+
+global.cheerio = cheerio;
+global.fs = fs;
+global.fetch = fetch;
+global.axios = axios;
+global.moment = moment;
