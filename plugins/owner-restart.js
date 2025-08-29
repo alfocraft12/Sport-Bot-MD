3132fbc8f179d60e12161116359a49efea7d167f
Alfonso Graterol <alfonsograterolpow@gmail.com>
2025-08-29 15:27:55 -0500

    Sport-Bot-MD

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -1,18 +1,14 @@
 import { spawn } from 'child_process'
-
 var handler = async (m, { conn, isROwner, text }) => {
-
-if (!process.send) throw 'Dont: node mini.js\nDo: node index.js'
 if (conn.user.jid == conn.user.jid) {
-await conn.reply(m.chat, '*🔄 EL BOT SE ESTA REINICIANDO ~ALFO~ 🔄*', m, rcanal, )
-process.send('reset')
+await conn.reply(m.chat, '*🔄 EL BOT SE ESTA REINICIANDO ~ALFO~ 🔄*', m, rcanal)
+// Usar el reloadHandler en lugar de process.send
+await global.reloadHandler(true).catch(console.error)
+await conn.reply(m.chat, '*✅ BOT REINICIADO CORRECTAMENTE*', m, rcanal)
 } else throw 'eh'
-
 }
 handler.help = ['restart']
 handler.tags = ['owner']
 handler.command = ['restart','reiniciar'] 
-
 handler.rowner = true
-
 export default handler
